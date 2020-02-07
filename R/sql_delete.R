e1db58cacc75d8d6d454245f3b85ab9f94f3aefa
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-02-07 12:10:55 +0100

    add sql_delete

---
+++ R/sql_delete.R
@@ -0,0 +1,41 @@
+#' @export
+#' @examples
+#' sql_delete(
+#'   table = "my_tbl",
+#'   con = con,
+#'   from = "my_value_table",
+#'   where = list("where 1", sql("my_tbl.id > 1")),
+#'   returning = list(`ret 1` = "ret_col", sql("now()"))
+#' )
+sql_delete <- function(table, con, from, where, returning = NULL) {
+  from <- sql_from_clause(from, con)
+
+  # create where clause
+  # character may be named, sql must not be named
+  if (!is_sql_chr_list(where, chr_names = NA, sql_names = FALSE)) {
+    abort("every element of where must be a bare character or unnamed bare SQL")
+  }
+
+  where_clause <- sql_clause_generator(
+    auto_name(where),
+    expr_sql = .x,
+    expr_chr = glue_sql("target.{`.y`} = source.{`.x`}", .con = con),
+    collapse = " AND "
+  )
+
+  # create returning clause
+  # character may be named, sql may be named
+  if (!is_sql_chr_list(returning, chr_names = NA, sql_names = NA) &&
+      !is.null(returning)) {
+    abort("every element of returning must be a bare character or named bare SQL")
+  }
+
+
+  glue_sql("
+    DELETE FROM {`table`} AS target
+     USING {`from`} AS source
+     WHERE {where_clause}
+     ", .con = con
+  ) %>%
+    sql_returning(returning, con)
+}
