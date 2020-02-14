5b4d97592fe116f3dd5d9c3c0f7628975fb75ebb
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-02-14 15:00:43 +0100

    add dbQuoteIdentifier for dplyr::sql

--- R/dbtools.R
+++ R/dbtools.R
@@ -10,3 +10,14 @@ glue::glue_sql
 #' @importFrom DBI SQL
 #' @export
 DBI::SQL
+
+#' @importClassesFrom RPostgres PqConnection
+#' @importMethodsFrom DBI dbQuoteIdentifier
+setMethod("dbQuoteIdentifier", c("PqConnection", "sql"), function(conn, x, ...) {
+  x
+})
+
+#' @importClassesFrom RSQLite SQLiteConnection
+setMethod("dbQuoteIdentifier", c("SQLiteConnection", "sql"), function(conn, x, ...) {
+  x
+})
