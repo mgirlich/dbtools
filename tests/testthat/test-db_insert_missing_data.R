92269339e54f90958cb1bef574005e79479fae77
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-02-18 09:20:36 +0100

    test with conflict_target as character instead of generated by `sql_unique_cols()`

--- tests/testthat/test-db_insert_missing_data.R
+++ tests/testthat/test-db_insert_missing_data.R
@@ -1,7 +1,7 @@
 test_db_insert_missing_data <- function(data,
                                         expected_returned,
                                         expected_state,
-                                        conflict_target = sql_unique_cols("id1", "id2"),
+                                        conflict_target = c("id1", "id2"),
                                         insert_cols = NULL,
                                         returning = SQL("*"),
                                         return_all = FALSE,
