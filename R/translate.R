bc5c7cbdf1ba951834a3669976834f3b664fa069
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-11-20 14:25:31 +0100

    add `translate_conflict()`

--- R/translate.R
+++ R/translate.R
@@ -1,8 +1,10 @@
+
+#' @noRd
+#' @param where
+#' * names = name of column in target table
+#' * character can be named
+#' * sql must not be named
 translate_where <- function(con, where, target_tbl = "target", source_tbl = "source") {
-  # compare to old `sql_clause_where()` -> need some checks etc
-  # * names = name of column in table to update
-  # * sql must not be named
-  # * character can be named
   where_list <- auto_name_chr(cast_to_list(where))
   sql_flag <- are_sql(where_list)
 
@@ -25,11 +27,12 @@ translate_where <- function(con, where, target_tbl = "target", source_tbl = "sou
   sql(purrr::flatten_chr(where_list))
 }
 
+#' @noRd
+#' @param where
+#' * names = columns to be updated
+#' * character can be named
+#' * sql must be named
 translate_update <- function(con, update, source_tbl = "source") {
-  # compare to old `sql_clause_update()` -> need some checks etc
-  # * names = columns to be updated
-  # * sql must be named
-  # * character can be named
   update_list <- auto_name_chr(cast_to_list(update))
   sql_flag <- are_sql(update_list)
 
@@ -43,6 +46,14 @@ translate_update <- function(con, update, source_tbl = "source") {
   )
 
   update_list
+}
+
+translate_conflict <- function(con, conflict) {
+  sql_clause_on_conflict(
+    con,
+    to_sql(conflict$conflict_target, con),
+    to_sql(conflict$conflict_action, con)
+  )
 }
 
 cast_to_list <- function(x) {
