eb2dc76a22c480975118af4f67744c026f3f3229
Maximilian Girlich <maximilian.girlich@metoda.com>
2020-11-24 08:07:40 +0100

    split sql_insert_conflict.R into sql_insert_missing.R and sql_upsert.R

--- R/sql_insert_missing.R
+++ R/sql_insert_missing.R
@@ -34,29 +34,3 @@ sql_insert_missing <- function(data,
     mode = mode
   )
 }
-
-
-#' SQL for upserting values
-#'
-#' @inheritParams sql_insert_missing
-#' @inheritParams sql_update
-#'
-#' @export
-sql_upsert <- function(data,
-                       table,
-                       con,
-                       conflict_target,
-                       update,
-                       insert_cols = NULL,
-                       returning = NULL,
-                       mode = "new") {
-  sql_insert(
-    data = data,
-    table = table,
-    con = con,
-    conflict = sql_do_update(conflict_target, update),
-    insert_cols = insert_cols,
-    returning = returning,
-    mode = mode
-  )
-}
